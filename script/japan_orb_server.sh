d33d198b9c47fa4dfef9c53ef106cfbd335aa69c
Drew Rife <dr5801@clipper.cs.ship.edu>
2016-12-06 10:43:00 -0500

    removed script

--- script/japan_orb_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-orbd -ORBInitialPort 1052 -port 1053 -ORBServerHost localhost &
-java FileSystemApp.FileSystemServer -ORBInitialPort 1052 -port 1053 -ORBServerHost 52.198.71.29 &
--- script/virginia_orb_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-orbd -ORBInitialPort 1052 -port 1053 -ORBServerHost localhost &
-java FileSystemApp.FileSystemServer -ORBInitialPort 1052 -port 1053 -ORBServerHost 54.146.131.219 &
